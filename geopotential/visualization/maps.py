9ef87b6f748a148ab1bf73200dd191c0db2efd02
El Hachemi SAYAH <elh.sayah@gmail.com>
2026-09-10 19:07:01 +0100

    Add interactive anomaly map visualization

---
+++ geopotential/visualization/maps.py
@@ -0,0 +1,32 @@
+import plotly.graph_objects as go
+
+
+def anomaly_map(work, x_col, y_col, value_col, xx, yy, zz, title):
+    fig = go.Figure()
+    fig.add_trace(go.Contour(
+        x=xx[0, :],
+        y=yy[:, 0],
+        z=zz,
+        colorscale='Turbo',
+        contours=dict(showlabels=True),
+        colorbar=dict(title=value_col),
+        name='Grid',
+    ))
+    fig.add_trace(go.Scatter(
+        x=work[x_col],
+        y=work[y_col],
+        mode='markers',
+        marker=dict(size=5, color='black'),
+        name='Stations',
+        text=[f'{v:.3f}' for v in work[value_col]],
+        hovertemplate='X=%{x}<br>Y=%{y}<br>Value=%{text}<extra></extra>',
+    ))
+    fig.update_layout(
+        title=title,
+        xaxis_title=x_col,
+        yaxis_title=y_col,
+        height=720,
+        margin=dict(l=40, r=40, t=70, b=40),
+    )
+    fig.update_yaxes(scaleanchor='x', scaleratio=1)
+    return fig
